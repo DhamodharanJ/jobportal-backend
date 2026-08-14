adb734cb6727ca4bb1dd225453bef3129584facc
DhamodharanJ <dhamodharanj8@gmail.com>
2026-08-14 17:58:00 +0530

    checking new branch changes

--- JobPortalApplication/src/main/java/jobportal/controller/AuthController.java
+++ JobPortalApplication/src/main/java/jobportal/controller/AuthController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/auth")
 @Tag(name = "Authentication", description = "User registration and login endpoints")
 public class AuthController {
-
+   // checking the new branch changes
     private final AuthService authService;
 
     public AuthController(AuthService authService) {
